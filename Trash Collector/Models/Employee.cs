c2cf7b946c327f982a14cde731417c6220054ec8
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-17 16:21:23 -0500

    Created an employee class, and added the assumed properties.

---
+++ Trash Collector/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> TodaysPickups { get; set; }
+        public int ZipCode { get; set; }
+        public bool TrashPickedUp { get; set; }
+    }
+}
