461924a46f45cbc5ee06288445b33323737e0186
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-17 16:12:22 -0500

    Created the customer class and added properties.

---
+++ Trash Collector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PickupDay { get; set; }
+        public string ExtraPickup { get; set; }
+        public double MoneyOwed { get; set; }
+
+    }
+}
