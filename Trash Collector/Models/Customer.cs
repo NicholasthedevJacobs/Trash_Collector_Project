c83bfe8dad541f05e1220f50aa15b574c7c9279e
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-23 21:05:29 -0500

    Changed the way variables are viewed.

--- Trash Collector/Models/Customer.cs
+++ Trash Collector/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -18,15 +19,24 @@ namespace Trash_Collector.Models
 
         [Key]
         public int Id { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         public string Address { get; set; }
+        [DisplayName("Zipcode")]
         public int ZipCode { get; set; }
+        [DisplayName("Pickup Day")]
         public string PickupDay { get; set; }
+        [DisplayName("Extra Pickup")]
         public string ExtraPickup { get; set; }
+        [DisplayName("Money Owed")]
         public double MoneyOwed { get; set; }
+        [DisplayName("Day service suspended")]
         public string StartDateEndOfPickups { get; set; }
+        [DisplayName("Last day of suspended service")]
         public string EndDateEndOfPickups { get; set; }
+        [DisplayName("Trash Collected")]
         public bool TrashCollected { get; set; }
 
         [ForeignKey("IdentityUser")]
